78b01be924178757c579f95711a120583cd87186
jmmon <jmmon93@gmail.com>
2022-07-14 10:44:31 -0400

    block class done; folder structure approximate

---
+++ blockchain.js
@@ -0,0 +1,15 @@
+const crypto = require("crypto");
+const SHA256 = (message) => crypto.createHash('sha256').update(message).digest('hex');
+
+class Block {
+	constructor(timestamp = "", data = []) {
+		this.timestamp = timestamp;
+		this.data = data;
+		this.hash = this.getHash();
+		this.prevHash = "";
+	}
+
+	getHash() {
+		return SHA256(this.prevHash + this.timestamp + JSON.stringify(this.data));
+	}
+}
