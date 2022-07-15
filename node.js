fcc658e53f51005250098679a06a5aa8dc9aea64
jmmon <jmmon93@gmail.com>
2022-07-15 10:23:35 -0400

    managed to make js version of our homework blockchain.py

---
+++ node.js
@@ -0,0 +1,127 @@
+const express = require('express');
+const crypto = require('node:crypto');
+const app = express();
+const port = 5000;
+
+const nodeIdentifier = crypto.randomUUID().replaceAll('-', '');
+const Blockchain = require('./BlockchainJs/blockchain.js');
+const blockchain = new Blockchain();
+
+console.log({nodeIdentifier});
+console.log(blockchain);
+
+getMethods = (obj) => Object.getOwnPropertyNames(obj).filter(item => typeof obj[item] === 'function')
+
+console.log(getMethods(blockchain));
+
+
+app.get('/chain', (req, res) => {
+	const response = {
+		chain: blockchain.chain,
+		length: blockchain.chain.length
+	};
+
+	res.status(200).send(JSON.stringify(response));
+});
+
+
+app.post('/transactions/new', (req, res) => {
+	const data = req.body;
+	console.log({values: data});
+	console.log('typeof values', typeof data);
+
+	if (!data) {
+		res.status(400).send("Missing Body");
+	}
+
+	const required = ["sender", "recipient", "amount"];
+
+	let missing = false;
+	for (const each of required) {
+		if (!Object.keys(data).includes(each)) {
+			missing = true;
+			console.log('Missing', each);
+			break;
+		}
+	}
+	if (missing) {
+		res.status(400).send('Missing Values');
+	}
+
+	const index = blockchain.newTransaction(data["sender"], data["recipient"], data["amount"]);
+
+	const response = {message: `Transaction will be added to block #${index}`};
+
+	res.status(201).send(JSON.stringify(response));
+});
+
+
+app.get('/mine', (req, res) => {
+	//add our mining reward
+	blockchain.newTransaction(
+		sender = "0",
+		recipient = nodeIdentifier,
+		amount = 1,
+	);
+	
+	//create the block and attempt to mine it
+	const block = blockchain.newBlock(proof = 0);
+	blockchain.proofOfWork(block);
+
+	const response = {
+		message: 			"New block mined!",
+		index: 				block["index"],
+		transactions: block["transactions"],
+		proof: 				block["proof"],
+		previousHash: block["previousHash"],
+	};
+
+	res.status(200).send(JSON.stringify(response));
+});
+
+
+app.post('/nodes/register', (req, res) => {
+	const data = req.body;
+	const nodes = data["nodes"];
+
+	if (!nodes || nodes == null) {
+		res.status(400).send('Error: Please supply a valid list of nodes');
+	}
+
+	for (const node of nodes) {
+		blockchain.registerNode(node);
+	}
+
+	const response = {
+		message: 'new nodes have been added!',
+		total_nodes: [...blockchain.nodes],
+	};
+
+	res.status(201).send(JSON.stringify(response));
+});
+
+
+app.get('/nodes/resolve', (req, res) => {
+	const replaced = blockchain.resolveConflict();
+
+	let response;
+	if (replaced) {
+		response = {
+			message: 'Our chain was replaced',
+			newChain: blockchain.chain
+		};
+	} else {
+		response = {
+			message: 'Our chain is authoritative',
+			chain: blockchain.chain
+		};
+	}
+
+	res.status(200).send(JSON.stringify(response));
+});
+
+
+
+app.listen(port, () => {
+	console.log(`node listening on port ${port}`);
+})
