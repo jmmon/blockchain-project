1e27cc4ef78051c277e61a2ea9cebcb7827a92f8
jmmon <jmmon93@gmail.com>
2022-07-15 16:27:11 -0400

    transaction class

--- BlockchainJs/blockchain.js
+++ BlockchainJs/blockchain.js
@@ -36,6 +36,34 @@ class Block {
 	}
 }
 
+class Transaction {
+	constructor(
+		from,
+		to,
+		value,
+		fee,
+		dateCreated,
+		data,
+		senderPubKey,
+		transactionDataHash,
+		senderSignature,
+		minedInBlockIndex,
+		transferSuccessful
+	) {
+		this.from = from;
+		this.to = to;
+		this.value = value;
+		this.fee = fee;
+		this.dateCreated = dateCreated;
+		this.data = data;
+		this.senderPubKey = senderPubKey;
+		this.transactionDataHash = transactionDataHash.toString();
+		this.senderSignature = senderSignature;
+		this.minedInBlockIndex = minedInBlockIndex;
+		this.transferSuccessful = transferSuccessful;
+	}
+}
+
 class Blockchain {
 	constructor() {
 		this.chain = [];
