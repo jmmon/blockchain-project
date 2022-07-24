c67fff91e26c71f9710d2a26fc2dfc13d45e8dce
jmmon <jmmon93@gmail.com>
2022-07-24 14:15:24 -0400

    playing with hashing on webworkers; kinda working so far

--- web workers testing/basic example hashing/child.js
+++ web workers testing/basic example hashing/child.js
@@ -1,3 +1,33 @@
-module.exports = function (input, callback) {
-	callback(null, `${input} BAR (${process.pid})`);
+/* 
+This should run the mining function, validate the hash, and return false or the hash ( i think ).
+So should take the current nonce, the data, and then should be able to perform the hash and return the value
+
+
+I guess the main function should just loop indefinitely, starting service workers. Once a service worker finds the correct validated hash, it should end the main thread loop  and end all service workers.
+*/
+
+const crypto = require("crypto");
+const SHA256 = (message) => crypto.createHash('sha256').update(message).digest('hex');
+
+const validProof = (hash, difficulty) => {
+	return hash.slice(0, difficulty) === "0".repeat(difficulty);
+}
+
+
+module.exports = function ({nonce, blockDataHash, difficulty}, callback) {
+	console.log({nonce, blockDataHash, difficulty});
+	const timestamp = new Date().toISOString();
+	const dataToHash = `${blockDataHash}|${timestamp}|${nonce}`;
+	const blockHash = SHA256(dataToHash);
+	
+	const result = validProof(blockHash, difficulty);
+	console.log(`${process.pid} - ${dataToHash} - ${result? `***********SUCCESS*********** - hash: ${blockHash}` : ''}`);
+
+	if (!result) {
+		callback(null, result);
+	} else {
+		callback(null, {dataToHash, blockHash});
+
+	}
+
 }
--- web workers testing/basic example hashing/main.js
+++ web workers testing/basic example hashing/main.js
@@ -2,94 +2,67 @@ const workerFarm = require('worker-farm');
 const FARM_OPTIONS     = {
 	maxConcurrentWorkers        : require('os').cpus().length
 , maxCallsPerWorker           : Infinity
-, maxConcurrentCallsPerWorker : 1
+, maxConcurrentCallsPerWorker : Infinity
 };
 const	workers = workerFarm(FARM_OPTIONS, require.resolve('./child'));
+// const	workers = workerFarm(require.resolve('./child'));
 
 
-const crypto = require("crypto");
-const SHA256 = (message) => crypto.createHash('sha256').update(message).digest('hex');
-
-// const defaultOptions = {
-	// 	workerOptions               : {}
-	// , maxCallsPerWorker           : Infinity
-// , maxConcurrentWorkers        : require('os').cpus().length
-// , maxConcurrentCallsPerWorker : 10
-// , maxConcurrentCalls          : Infinity
-// , maxCallTime                 : Infinity
-// , maxRetries                  : Infinity
-// , autoStart                   : false
-// , onChild                     : function() {}
-// };
-
-// let numberOfAttempts = 0;
-// const end = 1000;
-// for (let i = 0; i < end; i++) {
-	// 	const callback = (err, output) => {
-	// 		console.log(output);
-	// 		if (++numberOfAttempts == end) {
-	// 			workerFarm.end(workers);
-	// 		}
-	// 	}
-	// 	workers(`#${i} FOO`, callback);
-	// }
-
-
-const data = "blockDataHash";
+const blockDataHash = "blockDataHash";
+const difficulty = 2;
 let success = false;
 let nonce = 0;
 
+let blockCandidate = {
+	blockDataHash,
+	dateCreated: '',
+	nonce,
+	blockHash: '',
+};
+
+console.log('starting while loop');
+
+const loop = () => {
+	const input = {
+		nonce,
+		blockDataHash,
+		difficulty
+	}
+
+	const callback = (err, result) => {
+		//always called in/from the main thread
+		if (result) {
+			const {dataToHash, blockHash} = result;
+			const [data, timestamp, nonce] = dataToHash.split("|");
+
+			blockCandidate = {
+				...blockCandidate,
+				dateCreated: timestamp,
+				nonce,
+				blockHash
+			}
+
+			success = blockCandidate;
+
+			// onSuccess(success);
+		}
+	}
+	if (success) return success;
+
+	workers(input, callback);
+	nonce++; // increment for next loop
+}
+
+// setInterval(loop, 1);
+
 while (!success) {
-	const callback = (err, output) => {
-		console.log(output);
-		if (++numberOfAttempts == end) {
-			workerFarm.end(workers);
-		}
-	}
-	workers(`#${i} FOO`, callback);
+	success = loop();
 }
+onSuccess(success);
 
-// 
-
-const validProof = (hash, difficulty) => {
-	return hash.slice(0, difficulty) === "0".repeat(difficulty);
-}
-
-const mineBlock = (block) => {
-	let timestamp = new Date().toISOString();
-	let nonce = 0;
-	let data = block.blockDataHash+"|"+timestamp+"|"+nonce;
-	let hash = SHA256(data);
-	// process.stdout.write('Mining');
-	let maxZeroesFound = Array.from(hash.slice(0, block.difficulty)).filter(char => char === "0").length;
-	
-	while (!validProof(hash, block.difficulty)) {
-		timestamp = new Date().toISOString();
-		nonce += 1;
-		data = block.blockDataHash+"|"+timestamp+"|"+nonce;
-		hash = SHA256(data);
-
-
-  	// for logging:
-		const zeroesAtStartArray = Array.from(hash.slice(0, block.difficulty)).filter(char => char === "0");
-		if (zeroesAtStartArray.length > maxZeroesFound) {
-			maxZeroesFound = zeroesAtStartArray.length;
-		}
-		const zeroesAtStartString = zeroesAtStartArray.join('') + "-".repeat(block.difficulty - zeroesAtStartArray.length);
-
-		const maxDots = 20;
-		let dotsNumber = Math.round(nonce / (16 ** (block.difficulty - 1)));
-		while (dotsNumber > maxDots) {
-			dotsNumber -= maxDots;
-		}
-		process.stdout.write('Mining ' +  nonce + " : " + maxZeroesFound + " | " + zeroesAtStartString + " .".repeat(dotsNumber) + " .\033[0G");
-	}
-	process.stdout.write('\n');
-
-	return {
-		blockDataHash: block.blockDataHash,
-		dateCreated: timestamp,
-		nonce: nonce,
-		blockHash: hash,
-	};
+const onSuccess = (blockCandidate) => {
+	console.log(blockCandidate);
+	console.log('while loop has ended');
+	workerFarm.end(workers);
+	process.exit();
 }
