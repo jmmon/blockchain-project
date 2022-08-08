8cb705dc71b0215f8150cf6a46e805e67c4300b2
jmmon <jmmon93@gmail.com>
2022-08-08 07:54:46 -0400

    partial navbar with active state!; need to figure out data/wallet/session flow

--- wallet/express wallet base/index.js
+++ wallet/express wallet base/index.js
@@ -2,23 +2,27 @@ const fs = require("fs");
 const path = require("path");
 const express = require("express");
 const ejsLayouts = require("express-ejs-layouts");
-const session = require('express-session');
-const genuuid = require("uid-safe")
+const session = require("express-session");
+const genuuid = require("uid-safe");
 
 const favicon = require("serve-favicon");
+const e = require( "express" );
 
 const app = express();
-app.use(favicon(
-	path.join(__dirname, "public", "images", "favicon.ico"),
-	{ maxAge: 0 }
-));
-app.use(session({
-	genid: (req) => genuuid(18),
-	secret: 'keyboard cat',
-	resave: false,
-	rolling: true,
-	saveUninitialized: true,
-}));
+app.use(
+	favicon(path.join(__dirname, "public", "images", "favicon.ico"), {
+		maxAge: 0,
+	})
+);
+app.use(
+	session({
+		genid: (req) => genuuid(18),
+		secret: "keyboard cat",
+		resave: false,
+		rolling: true,
+		saveUninitialized: true,
+	})
+);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.set("view engine", "ejs");
@@ -32,33 +36,41 @@ if (!fs.existsSync(walletDirectory)) {
 	fs.mkdirSync(walletDirectory);
 }
 
-(async () => {
-	// const {default: { bip32, bip39, bjs }} = await import("./lib/walletUtils.js");
-	// console.log(typeof bip32);
-	// console.log(typeof bip39);
-	// console.log(typeof bjs);
-	const {default: { generateWallet }} = await import("./lib/walletUtils.js");
-	// console.log(typeof generateWallet);
 
+(async () => {
+	const {
+		default: {
+			generateWallet,
+			encrypt,
+			decrypt,
+			cryptPassword,
+			comparePassword,
+			deriveKeysFromMnemonic,
+		},
+	} = await import("./lib/walletUtils.js");
+	// console.log({generateWallet, encrypt, decrypt, cryptPassword, comparePassword});
 
 	//  Homepage
 	app.get("/", (req, res) => {
 		const id = req.session.id || null;
-		res.render(__dirname + "/views/index.html", {
-			id
+		drawView(res, "index", {
+			id,
+			active: "index",
 		});
 	});
 
 	//  Page for creating a wallet
 	app.get("/create", (req, res) => {
 		const id = req.session.id || null;
-		res.render(__dirname + "/views/create.html", {
-			id
+		drawView(res, "create", {
+			id,
+			active: "create",
 		});
+
 	});
 
 	//  Create endpoint
-	app.post("/create", (req, res) => {
+	app.post("/create", async (req, res) => {
 		console.log("~req", req.body);
 		const password = req.body.password;
 		const repeatPassword = req.body.confirmPassword;
@@ -77,60 +89,40 @@ if (!fs.existsSync(walletDirectory)) {
 		}
 
 		// Generate wallet from random mnemonic
-		const {mnemonic, privateKey, publicKey, address} = generateWallet();
-
+		const { mnemonic, privateKey, publicKey, address } = generateWallet();
+		
 		// TODO: Encrypt and save into session storage
+		const encryptSuccess = await encryptAndSave(mnemonic, password, address, req);
+		console.log({encryptSuccess})
 
-
-		// req.session.wallet = ;
-
-		drawView(res, "create", {
-			mnemonic,
-			privateKey, 
-			publicKey, 
-			address,
-			filename: undefined,
-			error: undefined,
-		});
-
-
-		// Encrypt and save as json file
-		// wallet.encrypt(password).then((jsonWallet) => {
-		// 	let filename =
-		// 		"UTC_JSON_WALLET_" +
-		// 		Math.round(+new Date() / 1000) +
-		// 		"_" +
-		// 		Math.random(10000, 10000) +
-		// 		".json";
-
-		// 	fs.writeFile(
-		// 		walletDirectory + filename,
-		// 		jsonWallet,
-		// 		"utf-8",
-		// 		(err) => {
-		// 			if (err) {
-		// 				drawView(res, "create", {
-		// 					mnemonic: undefined,
-		// 					jsonWallet: undefined,
-		// 					filename: null,
-		// 					error: "Problem writing to disk: " + err.message,
-		// 				});
-		// 			} else {
-		// 				drawView(res, "create", {
-		// 					mnemonic: wallet.mnemonic.phrase,
-		// 					jsonWallet: JSON.stringify(jsonWallet),
-		// 					filename: filename,
-		// 					error: undefined,
-		// 				});
-		// 			}
-		// 		}
-		// 	);
-		// });
+		if (encryptSuccess === true) {
+			drawView(res, "create", {
+				mnemonic,
+				privateKey,
+				publicKey,
+				address,
+				filename: undefined,
+				error: undefined,
+			});
+		} else {
+			drawView(res, "create", {
+				mnemonic: null,
+				privateKey: null,
+				publicKey: null,
+				address: null,
+				filename: undefined,
+				error: "Problem encrypting: " + err.message,
+			});
+		}
 	});
 
 	//load your wallet
 	app.get("/load", (req, res) => {
-		res.render(__dirname + "/views/load.html");
+		const id = req.session.id || null;
+		drawView(res, "load", {
+			id,
+			active: "load",
+		});
 	});
 
 	app.post("/load", (req, res) => {
@@ -173,7 +165,11 @@ if (!fs.existsSync(walletDirectory)) {
 
 	//recover wallet
 	app.get("/recover", (req, res) => {
-		res.render(__dirname + "/views/recover.html");
+		const id = req.session.id || null;
+		drawView(res, "recover", {
+			id,
+			active: "recover",
+		});
 	});
 
 	//recover wallet
@@ -221,64 +217,107 @@ if (!fs.existsSync(walletDirectory)) {
 	});
 
 	app.get("/balance", (req, res) => {
-		res.render(__dirname + "/views/balance.html");
+		const id = req.session.id || null;
+		drawView(res, "balance", {
+			id,
+			active: "balance",
+			balances: undefined,
+			error: undefined,
+		});
 	});
 
 	app.post("/balance", (req, res) => {
 		// fetch user data (filename and password)
-		const filename = req.body.filename;
 		const password = req.body.password;
+		// const wallet = req.session.wallet = {
+		// 	encryptedMnemonic,
+		// 	address,
+		// }
+
+		if (!req.session.wallet) {
+			drawView(res, "balance", {
+				wallets: undefined,
+				error: "Please load wallet from mnemonic, or create one."
+			});
+			return false;
+		}
+
+		// decrypt wallet and add to session
+		const encryptedMnemonic = req.session.wallet.encryptedMnemonic;
+		const decryptedMnemonic = decrypt(encryptedMnemonic, password);
+		console.log({decryptedWallet});
+
+		const result = deriveKeysFromMnemonic(decryptedMnemonic);
+
+		console.log({result});
+
+		drawView(res, "balance", {
+			active: "balance",
+			balances: "some balances",
+			error: undefined,
+		});
+
+
+
+		// const decryptSuccess = await encryptAndSave(mnemonic, password, address, req);
 
 		//  read the file
-		fs.readFile(
-			walletDirectory + filename,
-			"utf8",
-			async (err, jsonWallet) => {
-				if (err) {
-					drawView(res, "balance", {
-						wallets: undefined,
-						error: "Error with file writing",
-					});
-				}
+		// fs.readFile(
+		// 	walletDirectory + filename,
+		// 	"utf8",
+		// 	async (err, jsonWallet) => {
+		// 		if (err) {
+		// 			drawView(res, "balance", {
+		// 				wallets: undefined,
+		// 				error: "Error with file writing",
+		// 			});
+		// 		}
 
-				ethers.Wallet.fromEncryptedJson(jsonWallet, password)
-					.then(async (wallet) => {
-						// generate 5 wallets from your master key
+		// 		ethers.Wallet.fromEncryptedJson(jsonWallet, password)
+		// 			.then(async (wallet) => {
+		// 				// generate 5 wallets from your master key
 
-						let derivationPath = "m/44'/60'/0'/0/";
-						let wallets = [];
-						const NUMBER_OF_DERIVATIONS = 5;
+		// 				let derivationPath = "m/44'/60'/0'/0/";
+		// 				let wallets = [];
+		// 				const NUMBER_OF_DERIVATIONS = 5;
 
-						for (let i = 0; i < NUMBER_OF_DERIVATIONS; i++) {
-							let hdNode = ethers.utils.HDNode.fromMnemonic(
-								wallet.mnemonic.phrase
-							).derivePath(derivationPath + i);
-							let walletInstance = new ethers.Wallet(
-								hdNode.privateKey,
-								provider
-							);
-							let balance = await walletInstance.getBalance();
+		// 				for (let i = 0; i < NUMBER_OF_DERIVATIONS; i++) {
+		// 					let hdNode = ethers.utils.HDNode.fromMnemonic(
+		// 						wallet.mnemonic.phrase
+		// 					).derivePath(derivationPath + i);
+		// 					let walletInstance = new ethers.Wallet(
+		// 						hdNode.privateKey,
+		// 						provider
+		// 					);
+		// 					let balance = await walletInstance.getBalance();
 
-							wallets.push({
-								keypair: walletInstance,
-								balance: ethers.utils.formatEther(balance),
-							});
-						}
+		// 					wallets.push({
+		// 						keypair: walletInstance,
+		// 						balance: ethers.utils.formatEther(balance),
+		// 					});
+		// 				}
 
-						drawView(res, "balance", { wallets, error: undefined });
-					})
-					.catch((err) => {
-						drawView(res, "balance", {
-							wallets: undefined,
-							error: "Balance query error: " + err.message,
-						});
-					});
-			}
-		);
+		// 				drawView(res, "balance", { 
+		// 					wallets, 
+		// 					error: undefined 
+		// 				});
+		// 			})
+		// 			.catch((err) => {
+		// 				drawView(res, "balance", {
+		// 					wallets: undefined,
+		// 					error: "Balance query error: " + err.message,
+		// 				});
+		// 			});
+		// 	}
+		// );
 	});
 
 	app.get("/send", (req, res) => {
-		res.render(__dirname + "/views/send.html");
+		const id = req.session.id || null;
+		drawView(res, "send", {
+			id,
+			active: "send",
+		});
 	});
 
 	app.post("/send", (req, res) => {
@@ -338,6 +377,25 @@ if (!fs.existsSync(walletDirectory)) {
 
 	// Preset helper functions ===
 
+	// const unlockWallet = (password, req,) => {
+
+
+
+
+
+	// }
+
+	const encryptAndSave = (mnemonic, password, address, req) => {
+		const result = cryptPassword(password);
+		console.log({result});
+		const encryptedMnemonic = encrypt(mnemonic, result);
+		req.session.wallet = {
+			encryptedMnemonic,
+			address,
+		}; 
+		return true;
+	}
+
 	function drawView(res, view, data) {
 		res.render(__dirname + "/views/" + view + ".html", data);
 	}
@@ -345,5 +403,4 @@ if (!fs.existsSync(walletDirectory)) {
 	app.listen(3000, () => {
 		console.log("App running on http://localhost:3000");
 	});
-
 })();
--- wallet/express wallet base/lib/walletUtils.js
+++ wallet/express wallet base/lib/walletUtils.js
@@ -8,48 +8,55 @@ import bjs from 'bitcoinjs-lib';
 const purpose = "44"
 const coinType = "7789"
 
-const bcrypt = require('bcrypt');
+import bcrypt from "bcrypt";
+import Crypto from "crypto";
 
-const getPathFromPathObject = ({ account, change, index }) => `m/${purpose}'/${coinType}'/${account}'/${change}/${index}`;
+const generatePathFromObject = ({ account, change, index }) => `m/${purpose}'/${coinType}'/${account}'/${change}/${index}`;
 
 const getAddress = (publicKey) => bjs.payments.p2pkh({
 	pubkey: publicKey,
 }).address;
 
 
-const IV = "1ec514b8b807860c2be9f7e86a704511";
+const IV = Crypto.randomBytes(16);
 
-const encrypt = (value, passphrase) => {
-	let cipher = crypto.createCipheriv('aes-256-cbc', passphrase, IV);
-	let encrypted = cipher.update(value, 'utf8', 'base64');
+const padBuffer = (string, length = 32) => {
+	const buffFromString = Buffer.from(string);
+	return Buffer.concat([buffFromString], length);
+}
+
+const encrypt = (toEncrypt, passphrase) => {
+	let cipher = Crypto.createCipheriv('aes-256-cbc', padBuffer(passphrase), IV);
+	let encrypted = cipher.update(toEncrypt, 'utf8', 'base64');
 	encrypted += cipher.final('base64');
 	return encrypted;
 };
 
 const decrypt = (encrypted, passphrase) => {
-	let decipher = crypto.createDecipheriv('aes-256-cbc', passphrase, IV);
+	let decipher = Crypto.createDecipheriv('aes-256-cbc', padBuffer(passphrase), IV);
 	let decrypted = decipher.update(encrypted, 'base64', 'utf8');
 	decrypted += decipher.final('utf8');
 	return decrypted;
 };
 
  
-const cryptPassword = (password, callback) => {
-   bcrypt.genSalt(10, (err, salt) => {
-    if (err) 
-      return callback(err);
-
-    bcrypt.hash(password, salt, (err, hash) => {
-      return callback(err, hash);
-    });
-  });
+const cryptPassword = (
+		password,
+) => {
+	console.log('cryptPassword function runs');
+	return bcrypt.hashSync(password, 10);
 };
 
-const comparePassword = (plainPass, hashword, callback) => {
+const comparePassword = (
+		plainPass, 
+		hashword, 
+	// callback
+	) => {
    bcrypt.compare(plainPass, hashword, (err, isPasswordMatch) => {   
-       return err == null ?
-           callback(null, isPasswordMatch) :
-           callback(err);
+       return err || isPasswordMatch;
+			 	// return err == null ?
+        //    callback(null, isPasswordMatch) :
+        //    callback(err);
    });
 };
 
@@ -59,19 +66,15 @@ const generateWallet = () => {
 	const masterSeed = bip39.mnemonicToSeedSync(mnemonic);
 	const masterNode = bip32.fromSeed(masterSeed);
 
-	const accountPath = {
+	const path = generatePathFromObject({
 		account: "0",
 		change: "0",
 		index: "0"
-	};
-
-	const path = getPathFromPathObject(accountPath);
+	});
 
 	const privateKey = masterNode.derivePath(path).privateKey;
 	const publicKey = masterNode.derivePath(path).publicKey;
 	const address = getAddress(publicKey);
-
-	// console.log(address);
 
 	return {
 		mnemonic,
@@ -101,28 +104,36 @@ const saveWallet = () => {
 	
 }
 
-const loadWallet = () => {
-	
+const deriveKeysFromMnemonic = (mnemonic) => {
+	const masterSeed = bip39.mnemonicToSeedSync(mnemonic);
+	const masterNode = bip32.fromSeed(masterSeed);
+
+	// only generating the first account, (not channge,) first address
+	const path = generatePathFromObject({
+		account: "0",
+		change: "0",
+		index: "0"
+	});
+
+	const privateKey = masterNode.derivePath(path).privateKey;
+	const publicKey = masterNode.derivePath(path).publicKey;
+	const address = getAddress(publicKey);
+
+	return {
+		privateKey: privateKey.toString('hex'),
+		publicKey: publicKey.toString('hex'),
+		address
+	};
 }
-
-
-
-// const walletUtils = {
-// 	bip32,
-// 	bip39,
-// 	bjs
-// };
-
-// export default walletUtils;
-// export default {
-// 	bip32,
-// 	bip39,
-// 	bjs
-// };
 
 
 const walletUtils = {
 	generateWallet,
+	encrypt, 
+	decrypt, 
+	cryptPassword, 
+	comparePassword,
+	deriveKeysFromMnemonic,
 };
 
 export default walletUtils;
@@ -136,7 +147,5 @@ ESM scripts can only be imported (not required)
 CJS scripts cannot import (only require)
 ESM scripts can "default import" but not "named import"
 ESM scripts can require(CJS) but causes trouble
-
-
 
 */
