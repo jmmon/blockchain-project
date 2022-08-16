b1acd8064afe1a9ffdd7f1aeab54157900d20ee0
jmmon <jmmon93@gmail.com>
2022-08-16 09:22:44 -0400

    block index now matches array index of the chain: so genesis block now has index 0 and to look it up we want to search for block 0

--- node/routes/blocks.js
+++ node/routes/blocks.js
@@ -10,7 +10,10 @@ router.get("/", (req, res) => {
 // works:
 router.get("/:id", (req, res) => {
 	const blockchain = req.app.get("blockchain");
-	res.status(200).send(JSON.stringify(blockchain.chain[req.params.id - 1]));
+	if (req.params.id < 0 || req.params.id > blockchain.chain.length - 1) {
+		return res.status(404).send(JSON.stringify({errorMsg: "Block not found!"}));
+	}
+	res.status(200).send(JSON.stringify(blockchain.chain[req.params.id]));
 });
 
 module.exports = router;
