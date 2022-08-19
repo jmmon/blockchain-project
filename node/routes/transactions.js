2be46de5c43e76bb864d580764901cd6fb6d8c6e
jmmon <jmmon93@gmail.com>
2022-08-19 10:28:16 -0400

    sending transactions: bring in walletUtils for some validation: using validateFields function, validation comments, validatePublicKey, validate address is from publicKey, verifySignature; comments on propagate transaction

--- node/routes/transactions.js
+++ node/routes/transactions.js
@@ -1,3 +1,4 @@
+const walletUtils = import('../../walletUtils/index.js');
 const express = require("express");
 const router = express.Router();
 
@@ -42,7 +43,6 @@ router.post("/send", (req, res) => {
 	const blockchain = req.app.get('blockchain');
 	const signedTransaction = req.body;
 	console.log({signedTransaction});
-	const requiredFields = ["from", "to", "value", "fee", "dateCreated", "data", "senderPubKey", "transactionDataHash", "senderSignature"];
 
 
 		//TODO: validate transaction data!!
@@ -57,56 +57,60 @@ router.post("/send", (req, res) => {
 	}
 
 	//check for missing fields
-	let missing = [];
-	const incomingDataKeys = Object.keys(signedTransaction);
-	for (const each of requiredFields) {
-		if (!incomingDataKeys.includes(each)) {
-			missing.push(each);
-		}
-	}
-	if (missing.length > 0) {
-		return res.status(400).send(JSON.stringify({errorMsg: `Invalid transaction: missing fields '${missing.join(", ")}'`}));
+	const result = blockchain.validateFields(signedTransaction);
+	if (result.valid !== true) {
+			return res.status(400).send(JSON.stringify({errorMsg: `Invalid transaction: missing fields '${result.missing.join(", ")}'`}));
 	}
 
 
 	//check for invalid values :
+		// value >= 0
 	if (signedTransaction.value < 0) {
 		return res.status(400).send(JSON.stringify({errorMsg: `Invalid transaction: 'value' must be at least 0`}));
 	}
-
+		// fee >= minimum
 	if (signedTransaction.fee < blockchain.config.minTransactionFee) {
 		return res.status(400).send(JSON.stringify({errorMsg: `Invalid transaction: 'fee' must be at least ${blockchain.config.minTransactionFee}`}));
 	}
-
+		// created date is BEFORE now
 	const currentTime = Date.now();
 	if (Date.parse(signedTransaction.dateCreated) > currentTime) {
 		return res.status(400).send(JSON.stringify({errorMsg: `Invalid transaction: 'dateCreated' cannot be created in the future! Transaction created: ${signedTransaction.dateCreated}; Current dateTime: ${currentTime.toISOString()}`}));
 	}
 
-	if (!blockchain.addressIsValid(signedTransaction.to)) {
+		// valid to address
+	if (!blockchain.validateAddress(signedTransaction.to)) {
 		return res.status(400).send(JSON.stringify({errorMsg: `Invalid transaction: 'to' address is invalid!`}));
 	}
-	if (!blockchain.addressIsValid(signedTransaction.from)) {
+
+		// valid from address
+	if (!blockchain.validateAddress(signedTransaction.from)) {
 		return res.status(400).send(JSON.stringify({errorMsg: `Invalid transaction: 'from' address is invalid!`}));
 	}
 
-	// checks sender account balance >= value + fee
+		// sender account balance >= value + fee
+		// (NOT allowing sending of pending funds)
 	const balancesOfSender = blockchain.getBalancesOfAddress(signedTransaction.from);
-
 	if (balancesOfSender.confirmedBalance < (signedTransaction.value + signedTransaction.fee)) {
 		return res.status(400).send(JSON.stringify({errorMsg: `Invalid transaction: 'from' address does not have enough funds!`}));
 	}
 
-
-
-
-
 	//validate transaction public key ??
-	// TODO
+	if (!blockchain.validatePublicKey(signedTransaction.senderPubKey)) {
+		return res.status(400).send(JSON.stringify({errorMsg: `Public Key is invalid!`}));
+	}
+		// could validate the FROM address is derived from the public key
+	const hexAddress = walletUtils.getAddressFromCompressedPubKey(
+		signedTransaction.senderPubKey
+	);
+	if (signedTransaction.from !== hexAddress) {
+		return res.status(400).send(JSON.stringify({errorMsg: `FROM address is not derived from sender's public key!`}));
+	}
 
 	//validate signature is from public key
-	// TODO
-
+	if (!walletUtils.verifySignature(signedTransaction.transactionDataHash, signedTransaction.senderPubKey, signedTransaction.senderSignature)) {
+		return res.status(400).send(JSON.stringify({errorMsg: `Transaction signature is invalid!`}));
+	}
 
 	//check pendingTransactions for transaction with matching transactionDataHash (duplicate)
 	const newTransaction = blockchain.createTransaction(signedTransaction);
@@ -120,6 +124,7 @@ router.post("/send", (req, res) => {
 
 	// need to propagate the transaction to other nodes!
 	// TODO
+	// Go through peers and send post requests to alert them of the new transaction (so they can add it to their pending transactions?)
 
 
 	return res.status(200).send(JSON.stringify(newTransaction.transactionDataHash));
